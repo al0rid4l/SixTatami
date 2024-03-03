0ecf13c1bdc372639fd7a7e4f07f6fe184bb5322
al0rid4l <al0rid4l@gmail.com>
2024-03-03 23:15:05 +0800

    build: remove r2r

---
+++ build.ps1
@@ -0,0 +1,37 @@
+param([string]$c = $PWD, [string]$a, [string]$e, [string]$r = 'none', [switch]$z = $false)
+
+$cmd = 'dotnet publish'
+$argss = ''
+$releaseMode = 'Debug'
+
+# if ($c -match "csproj$") {
+# 	$argss += " $c"
+# }
+
+if ($a) {
+	$argss += " -p:Arch=$a"
+}
+
+if ($e) {
+	$argss += " -p:ExeFile=$e"
+}
+
+if ($r -notlike 'none') {
+	$argss += " -p:RID=$r"
+}
+
+if ($z) {
+	$releaseMode = 'Release'
+}
+
+$argss += " -c $releaseMode"
+
+echo "$releaseMode Build $c"
+echo "RID: $r"
+if ($c -match "csproj$") {
+	echo "$cmd $c $argss"
+	# Invoke-Expression "$cmd $c $argss"
+} else {
+	echo "$cmd $argss"
+	# Invoke-Expression "$cmd $argss"
+}
---
+++ build.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# 默认参数值
+rid='none'
+exe=
+arch=
+project=$(pwd)
+release='Debug'
+cmd='dotnet publish '
+args=''
+
+# 解析命令行选项
+while getopts ":r:e:a:c:z:" opt; do
+	case $opt in
+		r) rid=$OPTARG ;;
+		e) exe=$OPTARG ;;
+		a) arch=$OPTARG ;;
+		c) project=$OPTARG ;;
+		z) release='Release' ;;
+		\?)
+			echo "无效的选项: -$OPTARG" >&2
+			exit 1 ;;
+	esac
+done
+
+if [[ $arch ]]; then
+	args=$args"-p:Arch=$arch "
+fi
+
+if [[ $exe ]]; then
+	args=$args"-p:ExeFile=$exe "
+fi
+
+if [[ $rid ]] && [[ $rid != 'none' ]]; then
+	args=$args"-p:RID=$rid "
+fi
+
+args=$args" -c $release "
+
+echo "$release Build $project"
+echo "RID: $rid"
+
+if [[ $project =~ csproj$ ]]; then
+	echo "$cmd$project$args"
+else
+	echo "$cmd$args"
+fi
