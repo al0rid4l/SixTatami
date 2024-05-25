f352b4121a876d899ac415490a25523099cbaaf7
al0rid4l <al0rid4l@gmail.com>
2024-05-25 14:40:24 +0800

    feat: add TaggedEnum example

--- src/Examples/TaggedEnum/Name.cs
+++ src/Examples/TaggedEnum/Name.cs
@@ -2,12 +2,44 @@ using TaggedEnum;
 
 namespace Examples;
 
+[Tagged]
+public enum Book0 {
+
+	[Data("Computer Systems: A Programmer's Perspective")]
+	CSAPP,
+	[Data("Advanced Programming in the UNIX Environment")]
+	APUE,
+	[Data("Structure and Interpretation of Computer Programs")]
+	SICP
+}
+
 [Tagged<int>]
-public enum Name {
-	[Data<int>(22)]
-	a,
-	[Data<int>(33)]
-	b,
-	[Data<int>(44)]
-	c
+public enum Book1 {
+
+	[Data(1)]
+	CSAPP,
+	[Data<int>(2)]
+	APUE,
+	[Data(3)]
+	SICP
+}
+
+[Tagged]
+public enum Book2 {
+
+	[Data("Computer Systems: A Programmer's Perspective")]
+	CSAPP,
+	[Data] // Data is "APUE"
+	APUE,
+	[Data("Structure and Interpretation of Computer Programs")]
+	SICP
+}
+
+[Tagged(UseAll = true)]
+public enum Book3 {
+
+	[Data("Computer Systems: A Programmer's Perspective")]
+	CSAPP,
+	APUE, // Data is "APUE"
+	SICP // Data is "SICP"
 }
