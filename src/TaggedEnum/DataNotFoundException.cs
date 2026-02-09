d0c9731407680bb9ec994a3d70cbac68ee56226c
al0rid4l <al0rid4l@gmail.com>
2026-02-10 00:01:08 +0800

    fix: TaggedEnum error CS8954

--- src/TaggedEnum/DataNotFoundException.cs
+++ src/TaggedEnum/DataNotFoundException.cs
@@ -1,2 +1,3 @@
-namespace TaggedEnum;
+#pragma warning disable CA1050 // Declare types in namespaces
 public sealed class DataNotFoundException(string msg): Exception(msg);
+#pragma warning restore CA1050 // Declare types in namespaces
